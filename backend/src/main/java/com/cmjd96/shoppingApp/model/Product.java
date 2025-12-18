29f38526530b141381371157d771a9740169232a
madubashinii <nilakshimadubashini920@gmail.com>
2025-12-18 23:17:28 +0530

    update product Id genaration instead of manual entry

--- backend/src/main/java/com/cmjd96/shoppingApp/model/Product.java
+++ backend/src/main/java/com/cmjd96/shoppingApp/model/Product.java
@@ -18,7 +18,9 @@ import javax.persistence.*;
 public class Product {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String productName;
     private String productBrand;
     private double productPrice;
